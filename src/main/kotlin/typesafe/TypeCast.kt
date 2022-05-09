2a2ef30647e4cc58ddea03c5ac84dff839170ea2
techzealot <techzealot@foxmail.com>
2022-05-09 15:51:44 +0800

    补充智能类型转换多线程不变性

--- src/main/kotlin/typesafe/TypeCast.kt
+++ src/main/kotlin/typesafe/TypeCast.kt
@@ -25,3 +25,16 @@ fun testWhen(any: Any) {
 }
 
 fun getAny(): Any = if (Random.nextInt(10) > 5) 1 else "low than 5"
+
+//此处使用var会导致只能转换失效
+val s: String? = getString()
+
+//智能类型转换
+fun testVarSmartCast() {
+    if (s != null) {
+        //var s 的写法：error 可能在判空后在多线程下为null,只有在确定判空后不变的情况下才可转换
+        println(s.length)
+    }
+}
+
+fun getString(): String? = if (Random.nextInt(10) > 5) "test" else null
