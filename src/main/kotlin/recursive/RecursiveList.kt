3c8f10dc2bc09174e8afab229beefa5b9757fc5e
techzealot <techzealot@foxmail.com>
2022-06-08 16:45:21 +0800

    通过sealed class实现递归数据结构

---
+++ src/main/kotlin/recursive/RecursiveList.kt
@@ -0,0 +1,69 @@
+package recursive
+
+/**
+ * 实现递归数据结构
+ * IntList [1,[2,[3,[4,nil]]]]
+ */
+sealed class IntList {
+    object Nil : IntList() {
+        override fun toString(): String {
+            return "nil"
+        }
+    }
+
+    data class Element(val head: Int, val tail: IntList) : IntList() {
+        override fun toString(): String {
+            return "[$head,$tail]"
+        }
+    }
+}
+
+/**
+ * 实现解构
+ */
+operator fun IntList.component1(): Int? = when (this) {
+    is IntList.Element -> head
+    IntList.Nil -> null
+}
+
+operator fun IntList.component2(): Int? = when (this) {
+    is IntList.Element -> tail.component1()
+    IntList.Nil -> null
+}
+
+operator fun IntList.component3(): Int? = when (this) {
+    is IntList.Element -> tail.component2()
+    IntList.Nil -> null
+}
+
+operator fun IntList.component4(): Int? = when (this) {
+    is IntList.Element -> tail.component3()
+    IntList.Nil -> null
+}
+
+fun intListOf(vararg values: Int): IntList {
+    return when (values.size) {
+        0 -> IntList.Nil
+        else -> IntList.Element(values[0], intListOf(*values.slice(1 until values.size).toIntArray()))
+    }
+}
+
+/**
+ * sum
+ */
+fun IntList.sum(): Int = when (this) {
+    is IntList.Element -> head + tail.sum()
+    IntList.Nil -> 0
+}
+
+fun main() {
+    val intList = intListOf(1, 2, 3, 4)
+    println(intList)
+    val (a, b, c, d) = intList
+    println("$a $b $c $d")
+    println("sum of IntList:${intList.sum()}")
+    //List可以解构五个元素
+    val list = listOf(1, 2, 3, 4, 5, 6)
+    val (a1, a2, _, a4, a5) = list
+    println("$a1 $a2 $a4 $a5")
+}
