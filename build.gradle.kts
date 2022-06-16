c943081190d128357bc02ea4f1a46bea2cf9f799
techzealot <techzealot@foxmail.com>
2022-06-16 18:12:27 +0800

    升级kotest至5.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,13 @@ group = "com.techzealot"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenLocal()
     maven("https://repo1.maven.org/maven2/")
+    mavenCentral()
 }
 
 dependencies {
-    runtimeOnly(kotlin("reflect"))
+    implementation(kotlin("reflect"))
     // kotlin coroutine 确保与kotlin("jvm")使用相同版本的标准库 便于调试
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
@@ -28,11 +29,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
     //===test====
     testImplementation(kotlin("test"))
-    val kotestVersion = "5.3.0"
+    val kotestVersion = "5.3.1"
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
-    testImplementation("io.kotest:kotest-framework-datatest:5.2.0")
+    testImplementation("io.kotest:kotest-framework-datatest:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
     //mockk
     testImplementation("io.mockk:mockk:1.12.4")
